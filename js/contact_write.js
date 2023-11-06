15a09030c7387c6edb11c2f52e7217b4c1ebde80
JamieChen007 <107536976+JamieChen007@users.noreply.github.com>
2023-11-06 12:40:11 +1100

    feat/update write function

--- js/contact_write.js
+++ js/contact_write.js
@@ -1,16 +1,21 @@
 const text = document.getElementById("contact_title_subtitle");
 
-const textValue = "I am Jamie Chen!";
-
+const textValue = "I am ";
+const textArr = ["Jamie Chen.", "a full stack developer.", "a designer."];
 let index = 1;
+let i = 0;
 
 const write = () => {
-  text.innerHTML = textValue.slice(0, index);
+  text.innerHTML = textValue + textArr[i].slice(0, index);
 
   index++;
 
-  if (index > textValue.length) {
+  if (index > textArr[i].length) {
     index = 1;
+    i++;
+  }
+  if (i >= textArr.length) {
+    i = 0;
   }
 
   setTimeout(write, 250);
--- js/index_write.js
+++ js/index_write.js
@@ -1,16 +1,23 @@
 const text = document.getElementById("index_introduce_detail0");
 
-const textValue = "I Love Programing!";
+const textValue = "I Love ";
+
+const textArr = ["Programing!", "Design!"];
 
 let index = 1;
+let i = 0;
 
 const write = () => {
-  text.innerHTML = textValue.slice(0, index);
+  text.innerHTML = textValue + textArr[i].slice(0, index);
 
   index++;
 
-  if (index > textValue.length) {
+  if (index > textArr[i].length) {
     index = 1;
+    i++;
+  }
+  if (i >= textArr.length) {
+    i = 0;
   }
 
   setTimeout(write, 250);
